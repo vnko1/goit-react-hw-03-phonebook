d9007b3a8cff8674d7a0a9dafb8eea3e9f3c2a3b
Andrii Valenko <andreyvalenko@gmail.com>
2023-03-22 10:30:44 +0200

    style

--- src/components/phoneBook/contactForm/ContactForm.jsx
+++ src/components/phoneBook/contactForm/ContactForm.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
 import css from './ContactForm.module.css';
 
 const ContactForm = ({ onHandleSubmit }) => {
+  const nameId = nanoid();
+  const numberId = nanoid();
   return (
     <div className={css.container}>
       {' '}
       <form onSubmit={onHandleSubmit} className={css.form}>
-        <label className={css.label}>
+        <label className={css.label} htmlFor={nameId}>
           <p className={css.text}>Name</p>
           <input
             className={css.input}
@@ -14,10 +17,11 @@ const ContactForm = ({ onHandleSubmit }) => {
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            id={nameId}
             required
           />
         </label>
-        <label className={css.label}>
+        <label className={css.label} htmlFor={numberId}>
           <p className={css.text}>Number</p>
           <input
             className={css.input}
@@ -25,6 +29,7 @@ const ContactForm = ({ onHandleSubmit }) => {
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            id={numberId}
             required
           />
         </label>
